0c398e55d6f4691208f225ba3b542f34404588cf
Alex Bedard-Reid <alex.bedardreid@unity3d.com>
2022-11-16 09:34:31 -0800

    Added Base Scripts based on Miro
    [ADD] Added PackageWizardEditorWindow.cs, PackageGenerator.cs as frameworks to be added on to
    [ADD] Added FileWriterBase.cs to allow a structured way to generate & update files with Core
    [ADD] Added ConfigDataWriter.cs, PackageJsonWriter.cs & ReadmeWriter.cs to be used with the Package Wizard

---
+++ Packages/com.miniproject.core/Editor/Scripts/PackageWizard/File Writers/PackageJsonWriter.cs
@@ -0,0 +1,17 @@
+﻿using MiniProject.Core.Editor.Utilities;
+
+namespace MiniProject.Core.Editor.PackageWizard
+{
+    public class PackageJsonWriter : FileWriterBase
+    {
+        protected override void TryCreateFile(in string filePath, in string fileContents)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void TryUpdateFile(in string filePath, in string fileContents)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Packages/com.miniproject.core/Editor/Scripts/PackageWizard/PackageGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MiniProject.Core.Editor.PackageWizard
+{
+    public class PackageGenerator
+    {
+        public PackageGenerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Generate Functions
+        //================================================================================================================//
+
+        private void Generate()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CheckForExisting()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CreateNewPackage()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void TryCreateFiles()
+        {
+            throw new NotImplementedException();
+        }
+        private void TryCreateDirectories()
+        {
+            throw new NotImplementedException();
+        }
+        private void TryCreateAssemblyDefinitions()
+        {
+            throw new NotImplementedException();
+        }
+        //Post-Generate Functions
+        //================================================================================================================//
+
+        private void PostGenerate()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void UpdateManifests()
+        {
+            throw new NotImplementedException();
+        }
+        
+        //================================================================================================================//
+    }
+}
