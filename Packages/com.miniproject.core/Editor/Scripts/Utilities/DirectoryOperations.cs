df891f87f52114fe1df33fbe4d2e5a3350f85a90
Otto Oliveira <otto.oliveira@unity3d.com>
2022-11-23 10:06:47 -0800

    Directory and File operations

---
+++ Packages/com.miniproject.core/Editor/Scripts/Utilities/DirectoryOperations.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace MiniProject.Core.Editor.Utilities
+{
+    public class DirectoryOperations
+    {
+        public bool CreateFolder(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                Debug.LogError($"{path} already exists");
+                return false;
+            }
+
+
+            Directory.CreateDirectory(path);
+            return true;
+        }
+        
+        public bool DeleteFolder(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Debug.LogError($"{path} does not exists");
+                return false;
+            }
+
+
+            Directory.Delete(path);
+            return true;
+        }
+    }
+}
---
+++ Packages/com.miniproject.core/Editor/Scripts/Utilities/FileOperations.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace MiniProject.Core.Editor.Utilities
+{
+    public class FileOperations
+    {
+        /// <summary>
+        /// Moves the specified file to destination path.
+        /// </summary>
+        /// <param name="sourcePath">Path of file to move.</param>
+        /// <param name="destinationPath">Path of destination.</param>
+        public static bool Move(string sourcePath, string destinationPath)
+        {
+            if (!File.Exists(sourcePath))
+            {
+                Debug.LogError($"{sourcePath} not found");
+                return false;
+            }
+            File.Move(sourcePath, destinationPath);
+            return true;
+        }
+
+        /// <summary>
+        /// Deletes the specified file.
+        /// </summary>
+        /// <param name="filePath">The name of the file to be deleted.</param>
+        public static bool Delete(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                Debug.LogError($"{filePath} not found");
+                return false;
+            }
+
+            File.Delete(filePath);
+            return true;
+        }
+    }
+}
